967bb008c4441f62c2b984422a06716a8db08bba
Mitchell Holt <mitchellholt@Mitchells-Air.modem>
2024-06-14 13:08:19 +1000

    LES GOH treesitter

--- lua/after/treesitter.lua
+++ lua/after/treesitter.lua
@@ -1,27 +1,23 @@
-require("nvim-treesitter").setup {
-    ensure_install = { "core", "stable" },
-}
+require("nvim-treesitter.configs").setup {
+    highlight = {
+        enable = true,
+        -- languages to disable on
+        disable = { "latex", "matlab" },
 
--- require("nvim-treesitter.config").setup {
---     highlight = {
---         enable = true,
---         -- languages to disable on
---         disable = { "latex", "matlab" },
--- 
---         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
---         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
---         -- Using this option may slow down your editor, and you may see some duplicate highlights.
---         -- Instead of true it can also be a list of languages
---         additional_vim_regex_highlighting = false,
---     },
--- 
---     ensure_installed = {
---         "html",
---         "css",
---         "javascript",
---         "haskell",
---         "c",
---     },
--- 
---     sync_install = false,
--- }
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
+
+    ensure_installed = {
+        "html",
+        "css",
+        "javascript",
+        "haskell",
+        "c",
+    },
+
+    sync_install = false,
+}
