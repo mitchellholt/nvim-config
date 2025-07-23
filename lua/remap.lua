1baec92a661d8e2393f3201e701e9e19fc8dc0a4
Mitchell Holt <mitchellholt2002@gmail.com>
2025-07-23 14:58:26 +1000

    Keymap to delete lines from quickfix

--- lua/remap.lua
+++ lua/remap.lua
@@ -5,3 +5,25 @@ vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>")
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set("n", "<leader>nh", vim.cmd.nohl)
+
+
+-- quickfix list delete keymap
+function Remove_qf_item()
+  local curqfidx = vim.fn.line('.')
+  local qfall = vim.fn.getqflist()
+
+  if #qfall == 0 then return end
+
+  table.remove(qfall, curqfidx)
+  vim.fn.setqflist(qfall, 'r')
+
+  vim.cmd('copen')  
+
+  local new_idx = curqfidx < #qfall and curqfidx or math.max(curqfidx - 1, 1)
+
+  local winid = vim.fn.win_getid()
+  vim.api.nvim_win_set_cursor(winid, {new_idx, 0})
+end
+
+vim.cmd("command! RemoveQFItem lua Remove_qf_item()")
+vim.api.nvim_command("autocmd FileType qf nnoremap <buffer> dd :RemoveQFItem<cr>")
