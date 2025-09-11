1e0d6be5f59489aca1ce430e28fd22316e941acd
Mitchell Holt <mitchellholt2002@gmail.com>
2025-09-11 11:32:37 +1000

    Quickfix file motions

--- lua/remap.lua
+++ lua/remap.lua
@@ -12,31 +12,15 @@ vim.cmd([[
   noremap <C-j> :call append(line('.'), '')<CR>
 ]])
 
+-- Previous and next file in quickfix
+vim.cmd([[
+  noremap [f :cpf<CR>
+  noremap ]f :cnf<CR>
+]])
+
 
 -- select in line
 vim.cmd([[
   xnoremap il g_o^
   onoremap il :<C-u>normal vil<CR>
 ]])
-
-
--- quickfix list delete keymap
-function Remove_qf_item()
-  local curqfidx = vim.fn.line('.')
-  local qfall = vim.fn.getqflist()
-
-  if #qfall == 0 then return end
-
-  table.remove(qfall, curqfidx)
-  vim.fn.setqflist(qfall, 'r')
-
-  vim.cmd('copen')  
-
-  local new_idx = curqfidx < #qfall and curqfidx or math.max(curqfidx - 1, 1)
-
-  local winid = vim.fn.win_getid()
-  vim.api.nvim_win_set_cursor(winid, {new_idx, 0})
-end
-
-vim.cmd("command! RemoveQFItem lua Remove_qf_item()")
-vim.api.nvim_command("autocmd FileType qf nnoremap <buffer> dd :RemoveQFItem<cr>")
