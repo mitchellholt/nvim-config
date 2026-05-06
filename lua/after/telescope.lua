21607cee974dedf787e964642db0ddb0e06985ba
Mitchell Holt <mitchellholt2002@gmail.com>
2026-05-06 12:20:51 -0700

    Update telescope/treesitter interop

--- lua/after/telescope.lua
+++ lua/after/telescope.lua
@@ -7,14 +7,10 @@ vim.keymap.set('n', '<leader>pb', builtin.buffers)
 vim.cmd [[packadd cfilter]]
 
 require("telescope").setup({
-    defaults = {
-        mappings = {
-            n = {
-                ["<c-d>"] = require('telescope.actions').delete_buffer
-            },
-            i = {
-                ["<c-d>"] = require('telescope.actions').delete_buffer
-            }
-        }
+  defaults = {
+    mappings = {
+      n = { ["<c-d>"] = require('telescope.actions').delete_buffer },
+      i = { ["<c-d>"] = require('telescope.actions').delete_buffer }
     }
+  }
 })
